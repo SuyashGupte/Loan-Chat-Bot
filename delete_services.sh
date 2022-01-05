b78b8eb88f89b6c44c9a7b681881f1f743943f09
Suyash Gupte <gupte.suyash11@gmail.com>
2022-01-05 13:14:57 +0530

    Create delete_services.sh

---
+++ delete_services.sh
@@ -0,0 +1,9 @@
+servicename="cp-wstudio"
+ibmcloud resource service-instance-delete $servicename -f
+
+
+servicename="cp-wmachinelearning"
+ibmcloud resource service-instance-delete $servicename -f
+
+servicename="cp-wassistant"
+ibmcloud resource service-instance-delete $servicename -f
