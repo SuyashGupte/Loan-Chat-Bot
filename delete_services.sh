986c8b7323d843b7be49a5e6cbf4ac65e56e6995
Suyash Gupte <gupte.suyash11@gmail.com>
2022-01-11 20:32:45 +0530

    Update delete_services.sh

--- delete_services.sh
+++ delete_services.sh
@@ -6,4 +6,5 @@ servicename="cp-wmachinelearning"
 ibmcloud resource service-instance-delete $servicename -f
 
 servicename="cp-wassistant"
+ibmcloud resource service-key-delete Auto-generated%20service%20credentials -f
 ibmcloud resource service-instance-delete $servicename -f
