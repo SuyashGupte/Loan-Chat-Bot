6f7a25c30539c02ee0619201ae687b403bb0a3a7
Suyash Gupte <gupte.suyash11@gmail.com>
2022-01-20 23:55:19 +0530

    Update delete_services.sh

--- scripts/delete_services.sh
+++ scripts/delete_services.sh
@@ -1,11 +1,10 @@
 ibmcloud fn action delete ml
 
 servicename="cp-cos"
-ibmcloud resource service-instance-delete $servicename -f
+ibmcloud resource service-instance-delete $servicename -f --recursive
 
 servicename="cp-wmachinelearning"
-ibmcloud resource service-instance-delete $servicename -f
+ibmcloud resource service-instance-delete $servicename -f --recursive
 
 servicename="cp-wassistant"
-ibmcloud resource service-key-delete Auto-generated%20service%20credentials -f
-ibmcloud resource service-instance-delete $servicename -f
+ibmcloud resource service-instance-delete $servicename -f --recursive
